457585e5df5139434a9a107567c1011aca5f5907
Alishan Rahil <160777070+AlishanRahil@users.noreply.github.com>
2024-02-23 19:58:36 +0500

    \ question5.ts

--- question5.ts
+++ question5.ts
@@ -1,4 +1,4 @@
-// Question No 4 //
+// Question No 5 //
 let quotes = "The Future Belongs To Those Who Belives In The Beauty Of Their Dream";
 let famousperson = "Eleanor Roosevelt";
 console.log(quotes, "-", famousperson);
