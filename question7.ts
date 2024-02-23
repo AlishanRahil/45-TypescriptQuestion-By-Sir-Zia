f9dd6642d0adb336ccf2c8c78ee017fc2f96d27c
Alishan Rahil <160777070+AlishanRahil@users.noreply.github.com>
2024-02-23 20:00:08 +0500

    Add files via upload

---
+++ question7.ts
@@ -0,0 +1,21 @@
+// question 7 //
+let c = 5;
+let f = 3;
+console.log("The result of Addition =",c+f); // addition
+
+let o = 16;
+let y = 8; // Substraction
+console.log("The result Of Substraction = ",o-y);
+
+
+let s = 80;
+let i = 10; // division
+console.log("The result Of Division = ",s/i);
+
+
+let d = 8;
+let e = 1; // multipilication
+console.log("The result Of Multiplication = ",d*e);
+
+
+
