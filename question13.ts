2425a88c33c3fdca60b3467d77ccf76ac7014b4b
Alishan Rahil <160777070+AlishanRahil@users.noreply.github.com>
2024-02-23 20:30:39 +0500

    question13.ts

--- question13.ts
+++ question13.ts
@@ -1,6 +1,8 @@
 // question no 13 //
 
+// Stores Transportion In Array List //
 let  transportation = ["Car","Motorbike","Bus","Tempo"];
+// Print Each Transportion That i Would like to buy By Using For Loop //
 for(let i = 0; i <  transportation.length; i++){
     console.log(`i would like to own a ${transportation[i]}`);
 }
