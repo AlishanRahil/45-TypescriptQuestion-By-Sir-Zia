fcb6d2f88a46793040638a7b0f397f68ad09730f
Alishan Rahil <160777070+AlishanRahil@users.noreply.github.com>
2024-02-23 15:10:49 +0500

    Add files via upload

---
+++ question2.ts
@@ -0,0 +1,9 @@
+// 45 questions //
+// Question No 1//
+// # Store the person's name in a variable
+let names = "Eric";
+
+// # Print a message to the person
+console.log(`Hello ${names}, Would you like to learn some Python today?`);
+
+
