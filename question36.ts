9eba8494dc88617fe524dc1c1f9650c7c4795717
Alishan Rahil <160777070+AlishanRahil@users.noreply.github.com>
2024-02-25 02:47:21 +0500

    question36.ts

--- question36.ts
+++ question36.ts
@@ -1,7 +1,7 @@
 // Question No 36 //
 
 // T-Shirt: 
-// Make A function Of A shirt with ize,message
+// Make A function Of A shirt with size,message
 function make_shirt(size,message) {
     console.log(`Making a design for the ${size} T-Shirt ${message}`)
 };
